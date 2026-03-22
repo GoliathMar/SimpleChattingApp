0124c10c6c1b40e0890323697e68da28618384e3
GoliathMar <marcin.goliasz@gmail.com>
2026-03-22 02:38:04 +0100

    Fixed project config and added intial models

--- users-service/ConfigApp/asgi.py
+++ users-service/ConfigApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ChattingApp project.
+ASGI config for ConfigApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChattingApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConfigApp.settings')
 
 application = get_asgi_application()
--- users-service/manage.py
+++ users-service/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChattingApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConfigApp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
